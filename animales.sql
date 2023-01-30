635671180f009066e46678a5fe9949b112832f03
BryanMurga <83984607+BryanMurga@users.noreply.github.com>
2023-01-30 09:10:50 -0600

    Update animales.sql
    Modificación a la base de datos

--- animales.sql
+++ animales.sql
@@ -1,13 +1,13 @@
 create database apAnimales;
-
+use apAnimales;
 /* Creacion de bases de datos */
 create table animales(
-id int primary key auto_increment not null,
+id int primary key auto_increment,
 nombre varchar(20) not null,
 nombreCientifico varchar(50),
 habitad varchar(20),
 especie varchar (50) not null,
-peso int (3),
+peso double,
 dieta varchar(20),
 peligroExtincion tinyInt(1) not null
 );
@@ -20,7 +20,7 @@ select * from animales;
 
 /* Crear animales */
 insert into animales (nombre, nombreCientifico, habitad, especie,peso,dieta, peligroExtincion) 
-values ("Lobo Mexicano","Canis lupus baileyi","bosque", "C. lupus", "45kg","Carnivoro",1);
+values ("Lobo Mexicano","Canis lupus baileyi","bosque", "C. lupus", "45.5","Carnivoro",1);
 
 /* Actualizar datos */
 
